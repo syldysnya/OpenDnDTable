25967f71374a5e1dabf74b6464f85c145af08329
syldysnya <syldysnya@gmail.com>
2021-05-12 21:41:28 -0400

    test

--- app/models/dnd_campaign.rb
+++
@@ -1,6 +0,0 @@
-class DndCampaign < ApplicationRecord
-
-    validates :title, :adventure_type, :description, presence: true
-    validates :remote, :fifth_edition, inclusion: { in: [true, false]}
-
-end
--- app/models/reservation.rb
+++
@@ -1,6 +0,0 @@
-class Reservation < ApplicationRecord 
-
-    validates :game_date, :game_start, presence: true
-    validates :players_num, presence: true
-
-end
